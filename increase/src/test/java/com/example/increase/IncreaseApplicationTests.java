62ede4dccd013b369120255077b4578b7a7d7954
ariel <rivelinogol@gmail.com>
2019-12-31 20:27:35 -0300

    first commit.

---
+++ increase/src/test/java/com/example/increase/IncreaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.increase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncreaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
